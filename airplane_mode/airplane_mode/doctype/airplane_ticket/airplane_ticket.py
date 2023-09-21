3052769db3215465aae0e6ff7b363381e24faeef
gopi kathiriya <gopikathiriya2003gmail..com>
2023-09-21 18:53:07 +0530

    changes in json file

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,7 +1,10 @@
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class AirplaneTicket(Document):
+	def validate(self):
+		if self.status != "Boarded":
+			frappe.throw("Airpaln ticket cannot be submitted unless the status is boardes..")
 	def calculate_total_amount(self):
 		flight_price = self.flight_price
 		total_amount = flight_price + sum(add_ons.amount for add_ons in self.add_ons)
