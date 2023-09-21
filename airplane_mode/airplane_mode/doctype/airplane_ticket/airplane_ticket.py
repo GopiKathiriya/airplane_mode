b38ebfe7867df841c79d71118606a30b31c2b625
gopi kathiriya <gopikathiriya2003gmail..com>
2023-09-21 17:49:57 +0530

    changes in .py file

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,8 +1,14 @@
-# Copyright (c) 2023, airplane_mode and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class AirplaneTicket(Document):
-	pass
+    def before_save(self):
+        # Initialize total amount to the flight price
+        total_amount = self.flight_price or 0
+
+        
+        for item in self.get("Airplane Ticket Add-on Item"):
+            total_amount += item.amount or 0
+            frappe.throw("Hello")
+        # Set the calculated total amount
+        self.total_amount = total_amount
