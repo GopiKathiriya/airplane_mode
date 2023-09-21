2967d1502c1a8bac5b71163ccc67d4d1ff6cf768
gopi kathiriya <gopikathiriya2003gmail..com>
2023-09-21 18:06:39 +0530

    get the total amount

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,14 +1,10 @@
-import frappe
+# import frappe
 from frappe.model.document import Document
 
 class AirplaneTicket(Document):
-    def before_save(self):
-        # Initialize total amount to the flight price
-        total_amount = self.flight_price or 0
-
-        
-        for item in self.get("Airplane Ticket Add-on Item"):
-            total_amount += item.amount or 0
-            frappe.throw("Hello")
-        # Set the calculated total amount
-        self.total_amount = total_amount
+	def calculate_total_amount(self):
+		flight_price = self.flight_price
+		total_amount = flight_price + sum(add_ons.amount for add_ons in self.add_ons)
+		self.total_amount = total_amount
+	def before_save(self):
+		self.calculate_total_amount()
