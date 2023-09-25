bb071157cd6535f4dd80b330fbf978a8c8102837
gopi kathiriya <gopikathiriya2003gmail..com>
2023-09-25 18:06:16 +0530

    create one report

---
+++ airplane_mode/airplane_mode/report/add_on_popularity/add_on_popularity.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, airplane_mode and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["add on popularity"] = {
+	"filters": [
+
+	]
+};
