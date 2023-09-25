63c2b759bbcb8e1e2b523b5078f34b8c9b916a9e
gopi kathiriya <gopikathiriya2003gmail..com>
2023-09-25 15:59:22 +0530

    change on py file and create a report

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -3,8 +3,9 @@
 
 import frappe
 from frappe.website.website_generator import WebsiteGenerator
+from frappe import _
+
 
 class AirplaneFlight(WebsiteGenerator):
-	def on_submit(self):
-		if self.status == "Scheduled":
-			self.status = "Completed"
+	  pass
+	
